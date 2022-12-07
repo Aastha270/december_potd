38ffc12c5fc98481f2742d1477da1b8dc7eea3c2
Aastha270 <120048025+Aastha270@users.noreply.github.com>
2022-12-08 00:46:57 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+// RANGE SUM OF BST - 7 DECEMBER 2022
+/*
+
+class Solution {
+public:
+    
+    void solve(TreeNode* root, int low, int high, int &sum){
+        if(root == NULL)
+        return ;
+
+        solve(root->left, low, high, sum);
+        if(root->val >= low && root->val <= high) {
+            sum += root->val;
+        }
+        
+        solve(root->right, low, high, sum);
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        solve(root, low, high, sum);
+        return sum;
+    }
+};
+
+*/
